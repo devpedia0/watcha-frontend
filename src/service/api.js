1a0456916a010e9335cb90f4e9d5b2815c0bb6df
kyungyoonha <gkb10a@naver.com>
2020-11-23 13:18:58 +0900

    merge

--- src/service/api.js
+++ src/service/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
     // baseURL: "http://121.160.25.204:8080",
     baseURL: "http://222.111.195.42:8080",
-    //baseURL: "http://localhost:8080",
+    // baseURL: "http://localhost:8080",
     headers: {
         //Authorization: `Bearer ${token}`,
         //"Access-Control-Allow-Origin": "*",
