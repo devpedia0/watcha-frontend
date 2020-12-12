0ba6a940e9975f601319dcb608eb1cd516d7b6a3
hongcoder <honghs93@gmail.com>
2020-12-12 21:49:13 +0900

    에러주석

--- src/components/index.js
+++ src/components/index.js
@@ -1,39 +1,39 @@
 // Box
-export { default as BoxForm } from "./Box/BoxForm";
-export { default as BoxImg } from "./Box/BoxImg";
+export { default as BoxForm } from './Box/BoxForm';
+export { default as BoxImg } from './Box/BoxImg';
 // Card
-export { default as Card } from "./Card/Card";
-export { default as CardCollection } from "./Card/CardCollection";
-export { default as CardPoster } from "./Card/CardPoster";
-export { default as CardTag } from "./Card/CardTag";
-export { default as CardComment } from "./Card/CardComment";
+export { default as Card } from './Card/Card';
+export { default as CardCollection } from './Card/CardCollection';
+export { default as CardPoster } from './Card/CardPoster';
+export { default as CardTag } from './Card/CardTag';
+// export { default as CardComment } from "./Card/CardComment";
 // CardList
-export { default as CardList } from "./CardList/CardList";
-export { default as CardListForm } from "./CardList/CardListForm";
-export { default as CardListSlick } from "./CardList/CardListSlick";
+export { default as CardList } from './CardList/CardList';
+export { default as CardListForm } from './CardList/CardListForm';
+export { default as CardListSlick } from './CardList/CardListSlick';
 // Footer
-export { default as Footer } from "./Footer/Footer";
+export { default as Footer } from './Footer/Footer';
 // Form
-export { default as Input } from "./Form/Input";
-export { default as File } from "./Form/File";
-export { default as Textarea } from "./Form/Textarea";
-export { default as FormNav } from "./Form/FormNav";
-export { default as FormSection } from "./Form/FormSection";
-export { default as SelectCtg } from "./Form/SelectCtg";
-export { default as RadioYN } from "./Form/RadioYN";
-export { default as YearPicker } from "./Form/YearPicker";
-export { default as FormRoles } from "./Form/FormRoles";
-export { default as FormTags } from "./Form/FormTags";
+export { default as Input } from './Form/Input';
+export { default as File } from './Form/File';
+export { default as Textarea } from './Form/Textarea';
+export { default as FormNav } from './Form/FormNav';
+export { default as FormSection } from './Form/FormSection';
+export { default as SelectCtg } from './Form/SelectCtg';
+export { default as RadioYN } from './Form/RadioYN';
+export { default as YearPicker } from './Form/YearPicker';
+export { default as FormRoles } from './Form/FormRoles';
+export { default as FormTags } from './Form/FormTags';
 // Header
-export { default as Header } from "./Header/Header";
+export { default as Header } from './Header/Header';
 // Loader
-export { default as Loader } from "./Loader/Loader";
+export { default as Loader } from './Loader/Loader';
 // Modal
-export { default as Modal } from "./Modal/Modal";
-export { default as ModalParticipant } from "./Modal/ModalParticipant";
-export { default as ModalTag } from "./Modal/ModalTag";
-export { default as ModalBookmark } from "./Modal/ModalBookmark";
+export { default as Modal } from './Modal/Modal';
+export { default as ModalParticipant } from './Modal/ModalParticipant';
+export { default as ModalTag } from './Modal/ModalTag';
+export { default as ModalBookmark } from './Modal/ModalBookmark';
 // Setting
-export { default as Setting } from "./Setting/Setting";
+export { default as Setting } from './Setting/Setting';
 // Stars
-export { default as Stars } from "./Stars/Stars";
+export { default as Stars } from './Stars/Stars';
